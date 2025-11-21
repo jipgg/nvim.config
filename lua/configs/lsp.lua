1a73303695cbb7d1da08e4117a5346acae082b33
jip <83951137+jipgg@users.noreply.github.com>
2025-11-21 23:51:55 +0100

    lsp-overloads guard

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -28,6 +28,7 @@ create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if not client then return end
+                if not client.server_capabilities.signatureHelpProvider then return end
 
 		local lsp_overloads = require 'lsp-overloads'
 
